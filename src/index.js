0d57d6b5e6f0abc5cb3c9dc95e37b71ee3483235
Alex Schwindt <101961254+alex-schwindt@users.noreply.github.com>
2026-05-20 12:49:37 -0500

    Fix formatting of deploy instruction in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  *
  * - Run `npm run dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
+ * - Run `npm run deploy` to publish your worker 
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
